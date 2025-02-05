79fc36188f247d42acaa51c4a7bec721fb3f19de
abdulmoiz180 <moiza8684@gmail.com>
2025-02-05 15:01:09 +0500

    adding content

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import App from "./App";
-import theme from "./theme";
-import "./global.css";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./utils/i18n";
-const rootElement = document.getElementById("root");
-=======
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -17,8 +5,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 import './global.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './utils/i18n';
 const rootElement = document.getElementById('root');
->>>>>>> main
 const root = createRoot(rootElement);
 
 root.render(
